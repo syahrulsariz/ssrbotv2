48aee355cd67f5e79298161c89ebc86a27f4a5cd
syahrulsariz <75943517+syahrulsariz@users.noreply.github.com>
2021-01-31 11:38:29 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,91 @@
+const { create, Client } = require('@open-wa/wa-automate')
+const figlet = require('figlet')
+const options = require('./utils/options')
+const { color, messageLog } = require('./utils')
+const HandleMsg = require('./HandleMsg')
+
+const start = (aruga = new Client()) => {
+        console.log('------------------------------------------------')
+        console.log(color(figlet.textSync('SYAHRUL SR BOT', { horizontalLayout: 'full' })))
+        console.log('------------------------------------------------')
+    console.log(color('[DEV]'), color('BY SYAHRUL', 'yellow'))
+    console.log(color('[~>>]'), color('SERVER READY!', 'green'))
+
+    // Mempertahankan sesi agar tetap nyala
+    aruga.onStateChanged((state) => {
+        console.log(color('[~>>]', 'red'), state)
+        if (state === 'CONFLICT' || state === 'UNLAUNCHED') aruga.forceRefocus()
+    })
+
+    // ketika bot diinvite ke dalam group
+    aruga.onAddedToGroup(async (chat) => {
+	const groups = await aruga.getAllGroups()
+	// kondisi ketika batas group bot telah tercapai,ubah di file settings/setting.json
+	if (groups.length > groupLimit) {
+	await aruga.sendText(chat.id, `Maaf!, Grup yang bot tampung sudah penuh!`).then(() => {
+	      aruga.leaveGroup(chat.id)
+	      aruga.deleteChat(chat.id)
+	  }) 
+	} else {
+	// kondisi ketika batas member group belum tercapai, ubah di file settings/setting.json
+	    if (chat.groupMetadata.participants.length < memberLimit) {
+	    await aruga.sendText(chat.id, `Maaf!, Bot tidak bisa masuk kedalam grup karena jumlah member tidak melebihi ${memberLimit} orang!`).then(() => {
+	      aruga.leaveGroup(chat.id)
+	      aruga.deleteChat(chat.id)
+	    })
+	    } else {
+        await aruga.simulateTyping(chat.id, true).then(async () => {
+          await aruga.sendText(chat.id, `Halo! Saya SYAHRUL SR, ketik ${prefix}menu ntuk membuka menu bot!`)
+        })
+	    }
+	}
+    })
+
+    // ketika seseorang masuk/keluar dari group
+    aruga.onGlobalParicipantsChanged(async (event) => {
+        const host = await aruga.getHostNumber() + '@c.us'
+        const gChat = await aruga.getChatById(event.chat)
+        const pChat = await aruga.getContact(event.who)
+        const { contact, groupMetadata, name} = gChat
+        // kondisi ketika seseorang diinvite/join group lewat link
+        if (event.action === 'add' && event.who !== host) {
+            await aruga.sendTextWithMentions(event.chat, `Hallo @${event.who.replace('@c.us', '')} 👋\nSelamat datang di *Grup ${name}*\n═══════════════════\n*Nama  :*\n*Umur  :*\n*Askot :*\n═══════════════════\nSemoga nyaman disini, terima kasih!\n`)
+        }
+        // kondisi ketika seseorang dikick/keluar dari group
+        if (event.action === 'remove' && event.who !== host) {
+            await aruga.sendTextWithMentions(event.chat, `Yah @${event.who.replace('@c.us', '')} keluar, Grup ${name} merasa kehilangan:)`)
+        }
+    })
+
+    aruga.onIncomingCall(async (callData) => {
+        // ketika seseorang menelpon nomor bot akan mengirim pesan
+        await aruga.sendText(callData.peerJid, 'Maaf! Bot tidak dapat menerima panggilan telpon.\nNomor anda diblokir otomatis karena melakukan panggilan telpon!')
+        .then(async () => {
+            // bot akan memblock nomor itu
+            await aruga.contactBlock(callData.peerJid)
+        })
+    })
+
+    // ketika seseorang mengirim pesan
+    aruga.onMessage(async (message) => {
+        aruga.getAmountOfLoadedMessages() // menghapus pesan cache jika sudah 3000 pesan.
+            .then((msg) => {
+                if (msg >= 3000) {
+                    console.log('[aruga]', color(`Loaded Message Reach ${msg}, cuting message cache...`, 'yellow'))
+                    aruga.cutMsgCache()
+                }
+            })
+        HandleMsg(aruga, message)    
+    
+    })
+	
+    // Message log for analytic
+    aruga.onAnyMessage((anal) => { 
+        messageLog(anal.fromMe, anal.type)
+    })
+}
+
+//create session
+create(options(true, start))
+    .then((aruga) => start(aruga))
+    .catch((err) => new Error(err))
